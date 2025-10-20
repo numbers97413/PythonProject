a4836e455cb3117a15c47c253a5a700b98d7880f
peter <AIDiagnose@gmail.com>
2025-10-20 18:48:04 +0200

    i

---
+++ dubletter.py
@@ -0,0 +1,20 @@
+text = input("Enter numbers separated by spaces: ")
+number_strings = text.split()
+numbers = []
+for s in number_strings:
+    numbers.append(int(s))
+counts = {}
+for num in numbers:
+    if num in counts:
+        counts[num] = counts[num] + 1
+    else:
+        counts[num] = 1
+duplicates = []
+for num in counts:
+    if counts[num] > 1:
+        duplicates.append(num)
+if not duplicates:
+    result = "No duplicates\nList: " + str(numbers)
+else:
+    result = "Duplicates: " + str(duplicates) + "\nList: " + str(numbers)
+print(result)
