95642091cdf9bf69466dd221a6e2b5a176b123fd
peter <AIDiagnose@gmail.com>
2025-12-02 19:44:37 +0100

    i

--- 1.py
+++ 1.py
@@ -7,17 +7,29 @@ position = 50
 for rotation in rotationer:
     riktning = rotation[0] # Höger eller vänster
     lengd = len(rotation)
-    heltal = int(rotation[1:lengd]) # Antal steg
+    heltal = (rotation[1:lengd]) # Antal steg
+    if lengd == 4:
+        heltal = heltal[-2:]
+        heltal = int(heltal)
+    else:
+        heltal = int(heltal)
 
-    # print(rotation[0], heltal, rotation)
+    #print(rotation[0], heltal, rotation, lengd)
 
     if riktning == 'L':
-        position = position - heltal
+        print(position, riktning, heltal, rotation)
+        if position < heltal:
+            position = 100 - abs(position - heltal)
+        else:
+            position = position - heltal
 
     else:
+        print(position, riktning, heltal, rotation)
         position = position + heltal
+        if position > 99:
+            position = abs(100 - position)
 
-    print(riktning, position)
+    # print(position)
 
     if position == 0:
         nollor = nollor + 1
