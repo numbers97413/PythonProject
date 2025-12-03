049dc906a1dcf21a3a2b58ac56107fa24359019a
pidda2017 <52377787+pidda2017@users.noreply.github.com>
2025-12-03 05:50:02 +0100

    igy

--- 2/2.py
+++ 2/2.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
-ranges = Path('input.txt').read_text(encoding='utf-8').splitlines()
+# Each line becomes a list of strings, split by comma
+ranges = [
+    line.split(",")                  # split the line
+    for line in Path('input.txt').read_text(encoding='utf-8').splitlines()
+    if line.strip()                  # optional: skip empty lines
+]
 
+print(ranges)
 
+for range in ranges:
+    #print(range)
+    a, b = range.split("-")
+    print(a, b)
